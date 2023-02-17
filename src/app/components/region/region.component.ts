50ac7a1915073afee3e3d5c85b99924e0723d85a
nahuel <jnserrudo@gmail.com>
2023-02-17 17:26:57 -0300

    Se agrego imagenes para la descripcion de la mineria de datos y unas conclusiones por cada region

--- src/app/components/region/region.component.ts
+++ src/app/components/region/region.component.ts
@@ -18,6 +18,8 @@ export class RegionComponent {
 
   region_datos:any[]=[]
   premisas:any[]=[]
+  concluciones_premisas:any[]=[]
+  reglas:any[]=[]
 
 
   constructor(private aRoute:ActivatedRoute,private router:Router){
@@ -28,40 +30,34 @@ export class RegionComponent {
       this.url_img=`../assets/${this.region.toLocaleLowerCase()}.png`
       switch(this.region){
         case 'Noa':
-          this.descripcion="En la región del NOA, entre las principales relaciones existentes, se pudo descubrir que las personas que no ingerían elementos pertenecientes al grupo de las grasas, pero si ingerían tipos de carnes en una cantidad de 0 a 223.1 gramos, se puede concluir que con un 89,3% de confianza que esas personas no consumían pescados, pero si una cantidad de 0 a 50 gramos de alimentos pertenecientes al grupo de opcionales misceláneos. Otra relación importante se daba en el caso de las personas que no consumían pescados ni grasas y solo una cantidad de hasta 27.7 gramos de alimentos del grupo de aceites, las mismas con un 91,2% de confianza, se puede concluir que solo consumían hasta 50 gramos de alimentos del grupo de opcionales misceláneos."
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones noa"
+          this.descripcion=`En la región del Noa se pudo notar que al no consumir grasas, pero sí carnes, es muy probable que no se consuma pescados, pero si Opcionales y Misceláneos. Con el consumo de carnes, es muy probable que no se consuma pescados, pero si opcionales y misceláneos. Con el consumo de aceites, es muy probable que no se consuma pescados, pero si opcionales y misceláneos. Al no consumir grasas, pero sí dulces, es muy probable que no se consuma pescados, pero si opcionales y misceláneos. Al consumir verduras, es muy probable que no se consuma pescados, pero si opcionales y misceláneos.`
+          this.conclusiones="En la región del Noa, entre las principales relaciones, se encuentra influido el grupo de pescados y opcionales y misceláneos."
           break;
         case 'Nea':
-          this.descripcion="En la región del NEA, entre las principales relaciones existentes, se pudo descubrir que las personas que no ingerían pescados, pero si ingerían legumbres y cereales en una cantidad de 0 a 269.2 gramos, se puede concluir que con un 86,7% de confianza que esas personas consumían 2619.2 gramos del grupo relacionado con las bebidas y agua. Otra relación importante se daba en el caso de las personas que ingerían carnes, entre o y 223.1 gramos, las mismas con un 83,8% de confianza, se puede concluir que no ingerían pescados, pero sí elementos relacionados al grupo de bebidas y agua en una cantidad de 0 a 2619.2 gramos."
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones nea"
+          this.descripcion="En la región del Nea se pudo notar que el no consumo de pescados, conjunto al consumo de legumbres y cereales provoca un consumo en el grupo de bebidas y agua. Con determinado consumo de carnes, es muy probable que no se consuma pescados, pero si bebidas y agua. Con el no consumo de pescados, pero si consumo de grupos de aceites como así también de opcionales y misceláneos, es muy probable que se consuma Bebidas y Agua. Con el no consumo de grasas, pero si de aceites, es muy probable que se consuma Bebidas y Agua. Con el consumo del grupo de aceites y del grupo de bebidas y agua, es muy probable que no se consuma pescados, pero si opcionales y misceláneos."
+          this.conclusiones="En la región del Nea, entre las principales relaciones, se encuentra influido el grupo de pescados y el de bebidas y agua."
 
           break;
         case 'Centro':
-          this.descripcion=`En la region del centro se pudo notar que al no consumir pescados, pero si  legumbres y cereales, es muy probable que se consuman bebidas y agua.
+          this.descripcion=`En la región del Centro se pudo notar que al no consumir pescados, pero si  legumbres y cereales, es muy probable que se consuman bebidas y agua.
           Con el consumo de aceites, pero no de yogures y postres de leche, es muy probable que se consuman opcionales y misceláneos. Al no consumir pescados, pero si aceites, es muy probable que se consuman opcionales y misceláneos. Con el consumo de verduras y de dulces, es muy probable que se consuman opcionales y misceláneos.
           `
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones centro"
+          this.conclusiones="En la región del Centro, entre las principales relaciones, se encuentra influido el grupo de bebidas y aguas y el de opcionales y misceláneos."
 
           break;
         case 'Patagonia':
-          this.descripcion="En la región de la PATAGONIA, entre las principales relaciones existentes, se pudo descubrir que las personas que no ingerían pescados ni tipos de leche, se puede concluir que con un 88,1% de confianza que esas personas no consumían alimentos del grupo de yogures y postres de leche. Otra relación importante se da en el caso de las personas que no ingerían pescados, alimentos relacionados con yogures y postres de lecha ni Huevos, las mismas con un 81,2% de confianza, se puede concluir que no ingerían alimentos pertenecientes a las grasas."
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones patagonia"
+          this.descripcion="En la región de la Patagonia se pudo notar que al no consumir pescados ni leches, es muy probable que si se consuma Yogures y Postres de Leche. Al consumir leche, es muy probable que se consuma Yogures y Postres de Leche. Al no consumir pescados, huevos y yogures y postres de leche, es muy probable que no se consuman grasas. Al no consumir frutas, es muy probable que no se consuman pescados ni Yogures y Postres de Leche. Al no consumir grasas ni dulces, es muy probable que no se consuma Yogures y Postres de Leche."
+          this.conclusiones="En la región del Patagonia, entre las principales relaciones, se encuentra influido el grupo de yogures, postres de leche, pescados y grasas."
 
           break;
         case 'GBA':
-          this.descripcion="En la región del GBA, entre las principales relaciones existentes, se pudo descubrir que las personas que no ingerían elementos pertenecientes al grupo de yogures y postres de leche ni al grupo de huevos, se puede concluir que con un 81,7% de confianza que esas personas no consumían pescados ni grasas. Otra relación importante se daba en el caso de las personas que no consumían grasas ni frutas, las mismas con un 81,7% de confianza, se puede concluir que no consumían alimentos del grupo de opcionales misceláneos."
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones gba"
+          this.descripcion="En la región del GBA se pudo notar que al no consumir huevos ni yogures y postres de leche, es muy probable que no se consuman pescados ni grasas. Al no consumir grasas ni frutas, es muy probable que no se consuman pescados ni yogures y postres de leche. Al no consumir pescados ni frutas, es muy probable que no se consuman grasas. Al no consumir pescados ni quesos, es muy probable que no se consuman grasas. Con el consumo de frutas, es muy probable que no se consuman pescados."
+          this.conclusiones="En la región del GBA, entre las principales relaciones, se encuentra influido el grupo de pescados, grasas, yogures y postres de lece."
 
           break;
         case 'Cuyo':
-          this.descripcion="En la región del CUYO, entre las relaciones más interesantes, se pudo descubrir que las personas que ingerían elementos pertenecientes al grupo de legumbres y cereales hasta 238.5 gramos, con un 84,6% de confianza que esas personas no consumían pescados, pero si una cantidad de hasta 46.2 gramos de alimentos pertenecientes al grupo de opcionales misceláneos. Otra relación importante se daba en el caso de las personas que no consumían grasas y solo una cantidad de hasta 76.9 gramos de alimentos del grupo de dulces, las mismas con un 84% de confianza, se puede concluir que solo consumían hasta 46.2 gramos de alimentos del grupo de opcionales misceláneos y ninguna cantidad perteneciente al grupo de pescados."
-          this.resultados="El valor consumido expresado en gramos de los grupos de alimentos que influyen en otros, son:"
-          this.conclusiones="conclusiones cuyo"
+          this.descripcion="En la región del Cuyo se pudo notar que con el consumo de legumbres y cereales, es muy probable que no se consuman pescados, pero si opcionales y misceláneos. Con el consumo de aceites, es muy probable que se consuman opcionales y misceláneos. Al no consumir grasas, pero sí dulces, es muy probable que se consuman opcionales y misceláneos. Al no consumir yogures y postres de leche, pero si dulces, es muy probable que se consuman opcionales y misceláneos. Con el consumo de bebidas y agua, es muy probable que se consuman opcionales y misceláneos."
+          this.conclusiones="En la región del Cuyo, entre las principales relaciones, se encuentra influido el grupo de pescados y opcionales y misceláneos."
 
           break;
 
@@ -70,6 +66,8 @@ export class RegionComponent {
   }
 
   ngOnInit(){
+    window.scrollTo(0,0)
+
     for(let reg of datos){
       if(this.region.toLowerCase()==reg.region.toLowerCase()){
         this.region_datos=reg.resultados
@@ -78,17 +76,9 @@ export class RegionComponent {
       }
     }
     this.poner()
+    console.log(this.region_datos)
+    this.reglas=this.region_datos.map(reg=>[reg.Premises.replace("_"," Y ").replace("SdL","S de L"),reg.Conclusion.replace("_"," Y ").replace("SdL","S de L")])
 
-    this.premisas=this.region_datos.map(reg=>reg.Premises)
-    for(let p of this.premisas){
-      for(let i=0;i<p.length;i++){
-       let remplazar=''
-        if(p[i]=='='){
-
-        }
-      }
-    }
-    console.log("region",this.region_datos,this.premisas)
   }
   poner(){
     // for (let region of this.resultados){
